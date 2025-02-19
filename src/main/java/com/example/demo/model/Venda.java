81dc4d13cebccbad1064423e6943c0ac83d21db2
Felipedefreitasrossi283 <Felipedefreitasrossifreitas@gmail.com>
2025-02-19 16:40:38 -0300

    venda;

--- src/main/java/com/example/demo/model/Venda.java
+++ src/main/java/com/example/demo/model/Venda.java
@@ -1,16 +1,55 @@
 package com.example.demo.model;
 
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
+import lombok.Getter;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 
-@NoArgsConstructor
-@AllArgsConstructor
 public class Venda {
     int idVenda;
     LocalDateTime dataVenda;
     ArrayList<Produto> produtos = new ArrayList<Produto>();
     Cliente cliente;
+
+    public Venda(){
+    }
+
+    public Venda(int idVenda, LocalDateTime dataVenda, ArrayList<Produto> produtos, Cliente cliente) {
+        this.idVenda = idVenda;
+        this.dataVenda = dataVenda;
+        this.produtos = produtos;
+        this.cliente = cliente;
+    }
+
+    public int getIdVenda() {
+        return idVenda;
+    }
+
+    public void setIdVenda(int idVenda) {
+        this.idVenda = idVenda;
+    }
+
+    public LocalDateTime getDataVenda() {
+        return dataVenda;
+    }
+
+    public void setDataVenda(LocalDateTime dataVenda) {
+        this.dataVenda = dataVenda;
+    }
+
+    public ArrayList<Produto> getProdutos() {
+        return produtos;
+    }
+
+    public void setProdutos(ArrayList<Produto> produtos) {
+        this.produtos = produtos;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
 }
