3cfa2c8070811a3deee18def1e8adad86c8b0587
MykyBalb <mykaelbalbino06@gmail.com>
2025-02-19 16:16:28 -0300

    Atualização Venda

--- src/main/java/com/example/demo/model/Venda.java
+++ src/main/java/com/example/demo/model/Venda.java
@@ -1,11 +1,15 @@
 package com.example.demo.model;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 
+@Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class Venda {
