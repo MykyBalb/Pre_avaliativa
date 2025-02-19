4af2cc06050b9421bcdb800c38324e3c0acfba6d
Felipedefreitasrossi283 <Felipedefreitasrossifreitas@gmail.com>
2025-02-19 15:44:40 -0300

    cliente, produto e venda.

---
+++ src/main/java/com/example/demo/controller/ControllerCliente.java
@@ -0,0 +1,25 @@
+package com.example.demo.controller;
+
+import com.example.demo.database.BancoCliente;
+import com.example.demo.model.Cliente;
+
+import java.util.List;
+
+public class ControllerCliente {
+
+    BancoCliente bcCliente = new BancoCliente();
+
+    public void inserirNoBanco(Cliente c) {
+        bcCliente.insert(c);
+    }
+    public List<Cliente> pegarTodosOsclientes(){
+        return bcCliente.findAll();
+    }
+    public boolean atualizarCliente(Cliente c) {
+        return bcCliente.update(c);
+    }
+
+    public boolean deletarCliente(int idCliente){
+        return bcCliente.delete(idCliente);
+    }
+}
--- src/main/java/com/example/demo/model/Cliente.java
+++ src/main/java/com/example/demo/model/Cliente.java
@@ -5,11 +5,11 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@NoArgsConstructor
-@AllArgsConstructor
+
 @Getter
 @Setter
-
+@NoArgsConstructor
+@AllArgsConstructor
 public class Cliente {
 
     int idCliente;
